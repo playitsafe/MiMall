51b91e98f4a42eb42e74c7a661ffedffe418bfcf
aaron <playitsafe@qq.com>
2020-05-28 17:24:36 +1000

    modal transition bug fixed

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,24 +1,23 @@
 <template>
   <transition name="slide">
     <div class="modal" v-show="showModal">
-      <div class="mask">
-        <div class="modal-dialog">
-          <div class="modal-header">
-            <span>{{title}}</span>
-            <a href="javascript:;" class="icon-close" @click="$emit('cancel')"></a>
-          </div>
-          <div class="modal-body">
-            <slot name="body">
+      <div class="mask"></div>
+      <div class="modal-dialog">
+        <div class="modal-header">
+          <span>{{title}}</span>
+          <a href="javascript:;" class="icon-close" @click="$emit('cancel')"></a>
+        </div>
+        <div class="modal-body">
+          <slot name="body">
 
-            </slot>
-          </div>
-          <div class="modal-footer">
-            <a href="javascript:;" class="btn" v-if="btnType==1" @click="$emit('submit')">{{sureText}}</a>
-            <a href="javascript:;" class="btn" v-if="btnType==2" @click="$emit('cancel')">{{cancelText}}</a>
-            <div class="btn-group" v-if="btnType==3">
-              <a href="javascript:;" class="btn" @click="$emit('submit')">{{sureTetx}}</a>
-              <a href="javascript:;" class="btn" @click="$emit('cancel')">{{cancelText}}</a>
-            </div>
+          </slot>
+        </div>
+        <div class="modal-footer">
+          <a href="javascript:;" class="btn" v-if="btnType==1" @click="$emit('submit')">{{sureText}}</a>
+          <a href="javascript:;" class="btn" v-if="btnType==2" @click="$emit('cancel')">{{cancelText}}</a>
+          <div class="btn-group" v-if="btnType==3">
+            <a href="javascript:;" class="btn" @click="$emit('submit')">{{sureTetx}}</a>
+            <a href="javascript:;" class="btn" @click="$emit('cancel')">{{cancelText}}</a>
           </div>
         </div>
       </div>
